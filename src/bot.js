12b6c58ba6b290ac5598fa3fa9c571f80650f48c
nunoferr <nunoslmferreira@gmail.com>
2021-03-25 19:55:20 +0000

    new: commands parcing from user message implemented. new: sayHi command added.

--- src/bot.js
+++ src/bot.js
@@ -12,3 +12,27 @@ client.on('ready', () => {
 
 client.login(process.env.BOT_TOKEN);
 
+client.on('message', message => {
+    if (!message.content.startsWith(PREFIX)) return;
+    if (message.author.bot) return; // bots shouldn't execute commands
+    commandsMenu(message);
+});
+
+function commandsMenu(message) {
+    var commandsList = {
+        sayHi: sayHif
+    }
+    var [command, ...args] = message.content.substring(PREFIX.length).split(" ");
+    var params = [message, args];
+    commandsList[command].apply(null, params);
+}
+
+function sayHif(message, args = []) {
+    messages = [
+        'how is your day going?',
+        'hope you are having a wonderful day!',
+        'someone looks happy today!',
+        'want to hear a joke? Type: ' + PREFIX + 'joke' 
+    ];
+    message.channel.send(`Hello ${message.author}, ${messages[Math.floor(Math.random() * messages.length)]}`);
+}
